1001a342bf6f6d0533e0e20e1db3e0cee575eb27
Uzzal Roy <uzzalroy.acm@gmail.com>
2023-04-05 10:01:20 +0600

    Category controller define export arrow function

--- app/Controllers/CategoryController.js
+++ app/Controllers/CategoryController.js
@@ -5,7 +5,7 @@ const ApiResponse = require('../Response/BaseResponse');
 const router = express.Router();
 const categoryRepository = new CategoryRepository();
 
-router.get('/:id', async (req, res) => {
+export const getAllCategory = async (req, res) => {
     const user = await categoryRepository.findById(req.params.id);
   
     if (!user) {
@@ -13,14 +13,14 @@ router.get('/:id', async (req, res) => {
     }
   
     ApiResponse.success(res, user);
-  });
+  };
   
-  router.post('/', async (req, res) => {
+  export const saveCategory = async (req, res) => {
     const user = await categoryRepository.create(req.body);
     ApiResponse.success(res, user);
-  });
+  };
   
-  router.put('/:id', async (req, res) => {
+export const updateCategory = async (req, res) => {
     const user = await categoryRepository.update(req.params.id, req.body);
   
     if (!user) {
@@ -28,9 +28,9 @@ router.get('/:id', async (req, res) => {
     }
   
     ApiResponse.success(res, user);
-  });
+  };
   
-  router.delete('/:id', async (req, res) => {
+ export const deleteCategory = async (req, res) => {
     const result = await categoryRepository.delete(req.params.id);
   
     if (!result) {
@@ -38,7 +38,6 @@ router.get('/:id', async (req, res) => {
     }
   
     ApiResponse.success(res);
-  });
+  };
   
-  module.exports = router;
   
